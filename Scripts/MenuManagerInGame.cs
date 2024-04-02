6aa531dd259f6585b0c3d1f4d5d651b2b8fdb727
Duygu Eryılmaz <151764913+duygueryilmaz@users.noreply.github.com>
2024-04-02 23:00:44 +0300

    game project scripts added

---
+++ Scripts/MenuManagerInGame.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagerIngame : MonoBehaviour
+{
+    public GameObject ınGameScreen, pauseScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseButton()
+    {
+        Time.timeScale = 0;
+        ınGameScreen.SetActive(false);
+        pauseScreen.SetActive(true);
+    }
+
+    public void PlayButton()
+    {
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
+        ınGameScreen.SetActive(true);
+    }
+
+    public void RePlayButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    public void HomeButton()
+    {
+        Time.timeScale = 1;
+        DataManager.Instance.SaveData();
+        SceneManager.LoadScene(0);
+
+
+    }
+}
